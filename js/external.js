3bb038c1825ca51e486b5ac664037b48991efbe6
Ira Huff <ira.m.huff@gmail.com>
2023-10-13 16:19:07 -0500

    corrected some gramatical errors in external.js

--- js/external.js
+++ js/external.js
@@ -1,21 +1,21 @@
 "use strict";
-console.log("Hello from external JavaScript");
-alert('Welcome to my Website');
+console.log("Hello from external JavaScript.");
+alert('Welcome to my Website.');
 let color = prompt('What is your favorite color?');
 alert(color + ' is my favorite color to');
 //exorcise 3 part one
-let lm = prompt('how many days are you renting little mermaid?');
-let bb = prompt('how many days are you renting brother bear?');
-let herc = prompt('how many days are you renting hercules?');
-let cpd = prompt('what is the cost per day per movie?');
-alert('total cost will be ' + ((lm * cpd) + (bb * cpd) + (herc * cpd)) + ' bucks.');
+let lm = prompt('How many days are you renting little mermaid?');
+let bb = prompt('How many days are you renting brother bear?');
+let herc = prompt('How many days are you renting hercules?');
+let cpd = prompt('Hhat is the cost per day per movie?');
+alert('Total cost will be ' + ((lm * cpd) + (bb * cpd) + (herc * cpd)) n  + ' bucks.');
 //exorcise 3 part two
-let google = prompt('what is your hourly pay at google?');
-let amazon = prompt('what is your hourly pay at amazon?');
-let facebook = prompt('what is your hourly pay at amazon?')
-let googleHours = prompt('how many hours did you work at google?');
-let amazonHours = prompt('how many hours did you work at amazon');
-let facebookHours = prompt('how many hours did you work at facebook');
-alert('your pay will be ' + ((google * googleHours) + (amazon * amazonHours) + (facebook * facebookHours)) + ' bucks');
+let google = prompt('What is your hourly pay at google?');
+let amazon = prompt('What is your hourly pay at amazon?');
+let facebook = prompt('What is your hourly pay at amazon?')
+let googleHours = prompt('How many hours did you work at google?');
+let amazonHours = prompt('How many hours did you work at amazon?');
+let facebookHours = prompt('How many hours did you work at facebook?');
+alert('Your pay will be ' + ((google * googleHours) + (amazon * amazonHours) + (facebook * facebookHours)) + ' bucks.');
 
 
