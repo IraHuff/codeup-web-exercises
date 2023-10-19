b15fc4a84a6288666807ebbe98bc82ef3cb85048
Ira Huff <ira.m.huff@gmail.com>
2023-10-18 19:15:48 -0500

    adding for loops homework files

---
+++ js/for_loops.js
@@ -0,0 +1,26 @@
+function showMultiplicationTable(x){
+    for (let i = 1; i < 11; i++) {
+        let output = x * i
+        console.log(`${x} * ${i} = ${output}`)
+
+    }
+}
+//showMultiplicationTable(7)
+function randomOddOrEven(){
+    for (let i = 1; i < 11; i++){
+let random = Math.floor(Math.random() * 180) + 20;
+let oddOrEven = (random % 2 === 0) ? 'is even' : 'is odd';
+console.log(`${random} ${oddOrEven}`)
+}
+}
+//randomOddOrEven()
+
+function pyramid(){
+    for(let i=1;i<10;i++){
+        let repeat = i.toString()
+        console.log(repeat.repeat(i))
+
+    }
+}
+//pyramid()
+
