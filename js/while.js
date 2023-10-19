b0f121da190df71f281fadfbbeda735f1c57dc95
Ira Huff <ira.m.huff@gmail.com>
2023-10-18 21:01:50 -0500

    all problems appear to be solved

--- js/while.js
+++ js/while.js
@@ -1,5 +1,27 @@
-var i = 2;
-while (i < 65537) {
-    console.log(i);
-    i = i * 2;
+function timesTwo() {
+    let i = 2;
+
+    while (i < 65537) {
+        console.log(i);
+        i = i * 2;
+    }
 }
+
+//timesTwo()
+
+
+let allCones = Math.floor(Math.random() * 50) + 50;
+console.log("starting with " + allCones + ' cones')
+do {
+    let conesSold = Math.floor(Math.random() * 5) + 1;
+    if(allCones > conesSold) {
+        allCones -= conesSold
+        console.log(conesSold + " cones sold")
+    }else if(allCones < conesSold){
+        console.log("I cannot sell you " + conesSold + " I only have " + allCones)
+    }else {
+        console.log("I sold my last " + conesSold);
+        allCones -= conesSold
+    }
+
+}while (allCones > 0)
