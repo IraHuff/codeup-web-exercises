68649b4510813abed36ae59c5549e119aceb4455
Ira Huff <ira.m.huff@gmail.com>
2023-12-01 15:23:25 -0600

    just the search function i belive

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,37 +1,86 @@
 "use strict";
 (() => {
+
+    let lat
+    let lng
+
     //function to get weather data based on lat lon
     function getData(x, y) {
         fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${x}&lon=${y}&appid=${WE_KEY}&units=imperial`)
             .then(res => res.json())
             .then(data => {
-                console.log(data);
                 populateData(data)
             })
             .catch((e) => {
                 console.error(e);
             });
     }
+
     //function to populate city and weather cards
- function populateData(parse) {
-     let insert = document.querySelector('.cards')
-     let location = document.createElement('div')
-     location.innerText = parse.city.name
-     location.classList.add("city")
-     insert.appendChild(location)
-
-
-     for (let i = 0; i < parse.list.length; i += 8) {
-         let card = document.createElement('div');
-         let selection = parse.list[i];
-         card.innerHTML = `<p>Date: ${selection.dt_txt.slice(0,10)}</p>
-                           <p>Temp: ${selection.main.temp}</p>
+    function populateData(parse) {
+        let insert = document.querySelector('.cards')
+        for (let i = 0; i < parse.list.length; i += 8) {
+            let card = document.createElement('div');
+            let selection = parse.list[i];
+            card.innerHTML = `<p>Date: ${selection.dt_txt.slice(0, 10)}</p>
+                           <p>Temp: ${selection.main.temp.toString().slice(0, 2)}℉</p>
                            <img src="http://openweathermap.org/img/w/${selection.weather[0].icon}.png">
                            <p>Weather: ${selection.weather[0].description}</p>`
-         card.classList.add("card")
-         insert.appendChild(card)
-     }
- }
+            card.classList.add("card")
+            insert.appendChild(card)
+        }
+    }
 
- getData(39.74, -92.51)
+    function locationName(coordinates, apiKey) {
+        reverseGeocode(coordinates, apiKey).then(result => {
+            let insert = document.querySelector('.cards')
+            let location = document.createElement('div')
+            location.innerText = result
+            location.classList.add("city")
+            insert.appendChild(location)
+        })
+    }
+
+    lng = -92.48253417968736;
+    lat = 39.73524785044066;
+    const init =
+        {lng, lat}
+    locationName(init, MAP_KEY)
+    getData(39.74, -92.51)
+
+// draw map
+    mapboxgl.accessToken = MAP_KEY;
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: `mapbox://styles/mapbox/dark-v11`, // style URL
+        center: [-92.51, 39.74], // starting position [lng, lat]
+        zoom: 8, // starting zoom
+    });
+
+// Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
+
+
+    //draw marker
+    const marker = new mapboxgl.Marker({draggable: true}).setLngLat([-92.51, 39.74]).addTo(map);
+
+    function onDragEnd() {
+        const lngLat = marker.getLngLat();
+        lat = lngLat.lat;
+        lng = lngLat.lng;
+        let coords = {
+            lng,
+            lat
+        }
+        let insert = document.querySelector('.cards')
+        if (insert.firstElementChild !== null) {
+            while (insert.firstChild) {
+                insert.removeChild(insert.firstChild)
+            }
+        }
+        getData(lat, lng)
+        locationName(coords, MAP_KEY)
+    }
+
+    marker.on('dragend', onDragEnd);
 })()
